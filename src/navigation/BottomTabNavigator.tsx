0e32ad03570c317778d6baa5ea55a5e02416efad
lexx-project <passlexyganteng@gmail.com>
2025-11-21 18:04:50 +0700

    feat: add task for day 1 in week 4 ReactNative

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -3,11 +3,13 @@ import HomeScreen from '../screens/HomeScreen';
 import FontAwesome from '@react-native-vector-icons/fontawesome';
 import AboutScreen from '../screens/AboutScreen';
 import ProductListScreen from '../screens/ProductListScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 
 export type BottomTabsParamList = {
   Home: undefined;
   Products: undefined;
   About: undefined;
+  Profile: undefined;
 };
 
 const Tab = createBottomTabNavigator<BottomTabsParamList>();
@@ -24,21 +26,22 @@ export default function BottomTabNavigator() {
           ),
         }}
       />
-      <Tab.Screen
-        name="Products"
-        component={ProductListScreen}
-        options={{
-          tabBarIcon: ({ color }) => (
-            <FontAwesome name="th-list" size={26} color={color} />
-          ),
-        }}
-      />
+
       <Tab.Screen
         name="About"
         component={AboutScreen}
         options={{
           tabBarIcon: ({ color }) => (
             <FontAwesome name="user" size={28} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="user-circle" size={26} color={color} />
           ),
         }}
       />
